6d5fdaca2113f8aa8039626b95500cdb8501b1c5
Mario Flores Jr <m.flores4821@gmail.com>
2018-04-27 23:22:47 -0700

    fixed linter

--- lab-mario/lib/logger.js
+++ lab-mario/lib/logger.js
@@ -6,7 +6,7 @@ const logger = module.exports = winston.createLogger({
   level: 'info',
   format: winston.format.json(),
   transports: [
-    new winston.transports.File({ filename: 'combined.log' }),
+    new winston.transports.File({ filename: 'log.log', level: 'verbose' }),
     new winston.transports.File({ filename: 'error.log', level: 'error' }),
     new winston.transports.Console({ format: winston.format.simple(), level: 'info' }),
   ],
